c4e10a65c59c52c250351f2ac07ded7e0827bf66
Becca <cca.king@gmail.com>
2017-04-13 20:13:55 -0600

    commented out backend

--- controllers/message.js
+++ controllers/message.js
@@ -1,23 +1,23 @@
-var Message = require('../models/message') 
-var mongoose = require('mongoose');
- function create (req, res){
-    var message = new Message ({
-        message: req.body.text
-    })
-    message.save(function(err, result){
-        if (err) {
-            res.status(500)
-            res.json(err)
-        } else {
-            res.send(result);
-        }
-    })
-}
+// var Message = require('../models/message') 
+// var mongoose = require('mongoose');
+//  function create (req, res){
+//     var message = new Message ({
+//         message: req.body.text
+//     })
+//     message.save(function(err, result){
+//         if (err) {
+//             res.status(500)
+//             res.json(err)
+//         } else {
+//             res.send(result);
+//         }
+//     })
+// }
 
 
- module.exports = {
-    create : create,
-}
+//  module.exports = {
+//     create : create,
+// }
 
 
 
--- models/message.js
+++ models/message.js
@@ -1,10 +1,10 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
 
 
-var MessageSchema = Schema({
-	"message": String,
+// var MessageSchema = Schema({
+// 	"message": String,
 
-})
+// })
 
-module.exports = mongoose.model('Message', MessageSchema);
+// module.exports = mongoose.model('Message', MessageSchema);
