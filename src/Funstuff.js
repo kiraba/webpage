d182aadee55e3da4a83eee8bb5f7f847b7e8f697
Becca <cca.king@gmail.com>
2017-04-17 19:50:55 -0600

    ok last photo

--- src/Funstuff.js
+++ src/Funstuff.js
@@ -16,7 +16,7 @@ export default class Home extends Component{
         <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
           <div className="carousel-inner" role="listbox">
             <div className="carousel-item active">
-              <img className="d-block img-fluid" src="https://lh3.googleusercontent.com/oOwBPBHRAAmhL7fpQfmwhcC6l1d8kuDBOLIyfJ1mEH2UL_TnCDQfP9HfBraOtqfkCMo2e60ZpNtJwyQ9eUQMzE5X-livgL-wfdq5rOjvnBes4XfIfAjJrxBM-efEUM4BZtcz84TirEGL52ltD4esu-0wE0LAGw6DtMxiIo0JR0Z6RcHMhsVKrO6gRkGTpN87JmJGA5rH9R5EqoSqWwtchrFtGwnEMtMz_IrVNVM_mNFzesPKWdcIeVxo_PZhJEAygNCK34y1P0ZeS5ldnd_8W7A6rBD_YdSBWtyzh2sLLriYKIWRKPcSMwId5eXKGAJuEexcaB7X1gCJUWujBKbOHg0-kChUsVFB-W_bDQO2RywAxOLA0ihxrwMYifB-W_6Xtz_j-QLbHXOWamEtC1Ph2J_qhsLGGrKacyNQ0iRz8knwEZTQY8LMMt4Co5pFMx9-pX7lqz07u4zdCnU8SlGKsB91S7kITKMJW8O0ivKgrUEjeBICemLLyUiEXMFZ8prEf5fdGRNNjmylLFU9FrwQxOdm5U_GGCMS5-GjAMfxf_rwpCp9avnP5uCzVRHBB3DPf_tF8ZVMxzkzFsWCTYD0P8kePsIaM8bq1tBh3MptloPhtY33omvE=w1222-h324-no"  alt="First slide"></img>
+              <img className="d-block img-fluid" src="denalikidz.jpg"  alt="First slide"></img>
             </div>
             <div className="carousel-item">
               <img className="d-block img-fluid" src="gettotheCHOPPA.jpg" alt="Second slide"></img>
