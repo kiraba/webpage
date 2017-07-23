56b2aacdc5226ea0750c4d354cef5fdf9c006f98
Becca <cca.king@gmail.com>
2017-07-23 12:16:04 -0600

    rut date and open new tab on click

--- src/Funstuff.js
+++ src/Funstuff.js
@@ -34,12 +34,12 @@ export default class Home extends Component{
           <a href="#" className="title">
             Summer and Fall 2017 To Dos
           </a>
-          <a href="http://www.runnersedgemt.com/events/eleven-miles-to-paradise-trail-race/" className="list-group-item list-group-item-action">11 Miles To Paridise - May 21st -- Injured :(</a>
-          <a href="http://runchallis.com/" className="list-group-item list-group-item-action">River of No Return 50k - June 17th --- Injured :(</a>
-          <a href="https://www.seeley50.com/" className="list-group-item list-group-item-action">Seeley Lake 50K - July 14th - Finished!  5th Overall, 2nd Women's       Time: 5:38:10</a>
-          <a href="http://runtherut.com/" className="list-group-item list-group-item-action">The Rut Mountain Runs 28K - September 2nd</a>
-          <a href="http://snakeriveradventures.com/historic-sheep-creek-ranch/" className="list-group-item list-group-item-action">Raft the Snake and officiate a wedding! June 23rd - July 1st -- Done!</a>
-          <a href="#" className="list-group-item list-group-item-action">Two week outdoor job near Sun Valley, Idaho!  July 15th - 30th</a>
+          <a href="http://www.runnersedgemt.com/events/eleven-miles-to-paradise-trail-race/" target="_blank" className="list-group-item list-group-item-action">11 Miles To Paridise - May 21st -- Injured :(</a>
+          <a href="http://runchallis.com/" target="_blank" className="list-group-item list-group-item-action">River of No Return 50k - June 17th --- Injured :(</a>
+          <a href="https://www.seeley50.com/" target="_blank" className="list-group-item list-group-item-action">Seeley Lake 50K - July 14th - Finished!  5th Overall, 2nd Women's       Time: 5:38:10</a>
+          <a href="http://runtherut.com/" target="_blank" className="list-group-item list-group-item-action">The Rut Mountain Runs 28K - September 2nd</a>
+          <a href="http://snakeriveradventures.com/historic-sheep-creek-ranch/" target="_blank" className="list-group-item list-group-item-action">Raft the Snake and officiate a wedding! June 23rd - July 1st -- Done!</a>
+          <a href="#" target="_blank" className="list-group-item list-group-item-action">Two week outdoor job near Sun Valley, Idaho!  July 15th - 30th</a>
         </div>
         <p> I tend to take summer seriously...</p>
       </div>
