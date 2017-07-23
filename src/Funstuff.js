8630c22f0029c60062bc7b30fa64781e8532fc21
Becca <cca.king@gmail.com>
2017-07-23 13:46:59 -0600

    list alterations, bootstrap to react-bootstrap attempt

--- src/Funstuff.js
+++ src/Funstuff.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import './App.js';
 import $ from 'jquery';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
 import {
   BrowserRouter as Router,
   Route,
@@ -30,17 +31,17 @@ export default class Home extends Component{
           <a className="title" href="https://www.instagram.com/hijaiom/?hl=en"><img src="insta.png" className="icon" alt="responsive"/>Instagram</a>
           </p>
         </div>
-        <div className="list-group">
+        <ListGroup>
           <a href="#" className="title">
             Summer and Fall 2017 To Dos
           </a>
-          <a href="http://www.runnersedgemt.com/events/eleven-miles-to-paradise-trail-race/" target="_blank" className="list-group-item list-group-item-action">11 Miles To Paridise - May 21st -- Injured :(</a>
-          <a href="http://runchallis.com/" target="_blank" className="list-group-item list-group-item-action">River of No Return 50k - June 17th --- Injured :(</a>
-          <a href="https://www.seeley50.com/" target="_blank" className="list-group-item list-group-item-action">Seeley Lake 50K - July 14th - Finished!  5th Overall, 2nd Women's       Time: 5:38:10</a>
-          <a href="http://runtherut.com/" target="_blank" className="list-group-item list-group-item-action">The Rut Mountain Runs 28K - September 2nd</a>
-          <a href="http://snakeriveradventures.com/historic-sheep-creek-ranch/" target="_blank" className="list-group-item list-group-item-action">Raft the Snake and officiate a wedding! June 23rd - July 1st -- Done!</a>
-          <a href="#" target="_blank" className="list-group-item list-group-item-action">Two week outdoor job near Sun Valley, Idaho!  July 15th - 30th</a>
-        </div>
+          <ListGroupItem href="http://www.runnersedgemt.com/events/eleven-miles-to-paradise-trail-race/" target="_blank" className="list-group-item list-group-item-action">11 Miles To Paridise - May 21st -- Injured :(</ListGroupItem>
+          <ListGroupItem href="http://runchallis.com/" target="_blank" className="list-group-item list-group-item-action">River of No Return 50k - June 17th --- Injured :(</ListGroupItem>
+          <ListGroupItem href="https://www.seeley50.com/" target="_blank" className="list-group-item list-group-item-action">Seeley Lake 50K - July 14th - Finished!  5th Overall, 2nd Women's Time: 5:38:10</ListGroupItem>
+          <ListGroupItem href="http://runtherut.com/" target="_blank" className="list-group-item list-group-item-action">The Rut Mountain Runs 28K - September 2nd</ListGroupItem>
+          <ListGroupItem href="http://snakeriveradventures.com/historic-sheep-creek-ranch/" target="_blank" className="list-group-item list-group-item-action">Raft the Snake and officiate a wedding! June 23rd - July 1st -- Done!</ListGroupItem>
+          <ListGroupItem href="http://www.ercsv.org/" target="_blank" className="list-group-item list-group-item-action">Two week outdoor job near Sun Valley, Idaho!  July 15th - 30th</ListGroupItem>
+        </ListGroup>
         <p> I tend to take summer seriously...</p>
       </div>
     )
